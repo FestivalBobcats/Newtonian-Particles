da1326e6797caa9394d860514fa5f695d2ab6af1
Ben Hundley <ben.hundley@gmail.com>
2012-05-23 10:32:32 -0500

    buttz

--- public/javascripts/PS/forces/rotation.js
+++ public/javascripts/PS/forces/rotation.js
@@ -4,12 +4,10 @@ PS.Rotation = function(){
 PS.Rotation.prototype = {
 
   constructor: PS.Force,
-
-
-
+  
   apply: function(system){
     system.particles.forEach(function(particle){
-      
+
       var angularAccel = particle.acceleration.clone().divideScalar(particle.radius);
 
       particle.rotate(angularAccel);
